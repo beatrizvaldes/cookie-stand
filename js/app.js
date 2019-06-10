9070f1849e40d2fb96053414010bce6f522ad206
Beatriz Valdes <beatroz.vet@gmail.com>
2019-06-10 15:54:23 -0700

    First location added

--- js/app.js
+++ js/app.js
@@ -1,7 +1,16 @@
-var firstAndPike {
+var firstAndPike = {
     name: 'First and Pike',
     min: 23,
     max: 65, 
-    AverCookies: 6.3
-
+    AverCookies: 6.3,
+    
+     cookiesPerHourFn: function () {
+        var cookiesPerHour = [];
+        for(var i = 0; i < 15; i++ ){
+            var cookiesNumber = this.AverCookies * Math.floor(Math.random * (max -min) + min);
+        cookiesPerHour.push(cookiesNumber);
+        }
+       
+        return cookiesPerHour;
+    }
 };
